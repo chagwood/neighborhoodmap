48a2b00220cb51c026c107b266b49101e6afe7de
chagwood <chagwood@gmail.com>
2017-10-28 02:09:58 -0400

    Completed serialized promises for retrieving places data; changed pin color on selection; removed some place categories for speed efficiencies;

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import './styles.scss';
-
+/* ------------------------------------------------------------------ */
 var map;
 var infowindow;
 var placesService;
 var mapCenter = {lat: 38.958292, lng: -77.360039}; //initial map center and central point which the radius is measued from when finding places
 var currentPlaceMarkers = []; //array of map markers that are displayed at any given time
-var placeMarkersCache = {}; //object to cache info for a selected place
+var uniquePlaceMarkerIDs = {};
+var placeMarkersCache = {}; //object to cache info for a selected place (REMOVE)
+var placeMarkersData = {}; //object to cache info for a selected place
 var selectedPlaceName = ""; //currently selected place name
 var infoWindowMaxWidth = "200";
 var infoWindowImageHeight = "125";
-
+var defaultMarkerIcon = "http://maps.google.com/mapfiles/ms/icons/ylw-pushpin.png";
+var selectedMarkerIcon = "http://maps.google.com/mapfiles/ms/icons/grn-pushpin.png";
+var viewModel = "";
+/* ------------------------------------------------------------------ */
 /* name attribute is used in Google Places; label for navigation */
 var placesList = [{
-    name: "airport",
-    label: "Airports"
-}, {
-    name: "aquarium",
-    label: "Aquariums"
-}, {
     name: "art_gallery",
     label: "Art Galleries"
 }, {
@@ -36,17 +35,8 @@ var placesList = [{
     name: "beauty_salon",
     label: "Beauty Salons"
 }, {
-    name: "book_store",
-    label: "Book Stores"
-}, {
-    name: "bus_station",
-    label: "Bus Stations"
-}, {
     name: "cafe",
     label: "Cafes"
-}, {
-    name: "car_dealer",
-    label: "Car Dealers"
 }, {
     name: "car_rental",
     label: "Car Rentals"
@@ -68,9 +58,6 @@ var placesList = [{
 }, {
     name: "dentist",
     label: "Dentists"
-}, {
-    name: "department_store",
-    label: "Department Stores"
 }, {
     name: "doctor",
     label: "Doctors"
@@ -99,14 +86,8 @@ var placesList = [{
     name: "hardware_store",
     label: "Hardware Stores"
 }, {
-    name: "home_goods_store",
-    label: "Home Goods Stores"
-}, {
     name: "hospital",
     label: "Hospitals"
-}, {
-    name: "jewelry_store",
-    label: "Jewelry Stores"
 }, {
     name: "laundry",
     label: "Laundromats"
@@ -120,23 +101,14 @@ var placesList = [{
     name: "lodging",
     label: "Lodging"
 }, {
-    name: "meal_delivery",
-    label: "Meal Delivery"
-}, {
     name: "meal_takeaway",
     label: "Meal Take-out"
-}, {
-    name: "movie_rental",
-    label: "Movie Rentals"
 }, {
     name: "movie_theater",
     label: "Movie Theaters"
 }, {
     name: "museum",
     label: "Museums"
-}, {
-    name: "night_club",
-    label: "Night Clubs"
 }, {
     name: "park",
     label: "Parks"
@@ -162,9 +134,6 @@ var placesList = [{
     name: "school",
     label: "Schools"
 }, {
-    name: "shoe_store",
-    label: "Shoe Stores"
-}, {
     name: "shopping_mall",
     label: "Shopping Malls"
 }, {
@@ -174,16 +143,13 @@ var placesList = [{
     name: "storage",
     label: "Storage"
 }, {
-    name: "transit_station",
-    label: "Transit Stations"
-}, {
     name: "veterinary_care",
     label: "Veterinary Clinics"
 }, {
     name: "zoo",
     label: "Zoos"
 }];
-
+/* ------------------------------------------------------------------ */
 function initMap() {
     map = new google.maps.Map(document.getElementById('map-container'), {
         center: mapCenter,
@@ -251,27 +217,33 @@ function initMap() {
             }]
         }] //end of styles array
     });
-
-    
     infowindow = new google.maps.InfoWindow();
     placesService = new google.maps.places.PlacesService(map);
     google.maps.event.trigger(map, 'resize');
     google.maps.event.addListener(map, "click", function(event) {
         infowindow.close();
+        resetMapMarkers();
     });
-
 } //end of initMap
 window.initMap = initMap; //must set this to window or initial initMap callback doesn't work
-
+/* ------------------------------------------------------------------ */
 function AppViewModel() {
     var self = this;
+    self.categoriesLoaded = ko.observable(0);
+    self.pinsDisplayed = ko.observable(0);
+    self.totalCategories = placesList.length;
     self.places = ko.observableArray(placesList);
+    self.incrementLoadCounter = function() {
+        this.categoriesLoaded(this.categoriesLoaded() + 1);
+    };
+    self.incrementPinCounter = function() {
+        this.pinsDisplayed(this.pinsDisplayed() + 1);
+    };
     self.displayPlaces = function() {
         clearMapMarkers();
         UIkit.offcanvas("#ch-offcanvas").hide();
         selectedPlaceName = this.name;
         if(placeMarkersCache[selectedPlaceName] == undefined) {
-            this.markers = new Array();
             placesService.nearbySearch({
                 location: mapCenter,
                 radius: 5000,
@@ -286,7 +258,90 @@ function AppViewModel() {
         clearMapMarkers();
     };
 }
+/* ------------------------------------------------------------------ */
+function loadInitialPlaces() {
+    //load the modal
+    UIkit.modal("#wait-overlay").show()
 
+    //delay is used so we don't get limit exceeded errors from google apis
+    const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+    var promiseChain = placesList.reduce(function(promise, place) {
+      return promise.then(function(result) {
+        return Promise.all([delay(500), getPlacesForType(place.name)]);
+      })
+    }, Promise.resolve())
+
+    promiseChain.then(function(result){
+        //close the modal
+        UIkit.modal("#wait-overlay").hide();
+        //drop all the pins...
+    });
+
+    /*
+    var promiseArray = [];
+    for(var p = 0; p < placesList.length; p++) {
+        promiseArray.push(getPlacesForType(placesList[p].name))
+        //
+        placesService.nearbySearch({
+            location: mapCenter,
+            radius: 5000,
+            type: placesList[p].name
+        }, function(results, status){
+            var currentPlaceType = "";
+            console.log("RESULTS: " + results.length);
+            for(var i = 0; i < results.length; i++) {
+                for(var x = 0; x < results[i].types.length; x++){
+                    currentPlaceType = results[i].types[x];
+                    if(placeMarkersData[currentPlaceType] == undefined) {
+                        placeMarkersData[currentPlaceType] = new Array();
+                    }
+                    placeMarkersData[currentPlaceType].push(results[i]);
+                    createMapMarker(results[i]);
+                }
+            }
+        });
+        //
+    }
+    */
+}
+/* ------------------------------------------------------------------ */
+function getPlacesForType(type) {
+    return new Promise(function(resolve, reject) {
+        placesService.nearbySearch({
+            location: mapCenter,
+            radius: 5000,
+            type: type
+        }, function(results, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                var currentPlaceType = "";
+                // resolve results upon a successful status
+                for(var i = 0; i < results.length; i++) {
+                    //console.log(results);
+                    for(var x = 0; x < results[i].types.length; x++){
+                        currentPlaceType = results[i].types[x];
+                        if(placeMarkersData[currentPlaceType] == undefined) {
+                            placeMarkersData[currentPlaceType] = new Array();
+                        }
+                        placeMarkersData[currentPlaceType].push(results[i]);
+                        if(uniquePlaceMarkerIDs[results[i].id] == undefined) {
+                            createMapMarker(results[i]);
+                            uniquePlaceMarkerIDs[results[i].id] = results[i].id;
+                            viewModel.incrementPinCounter();
+                        }
+                        
+                    }
+                }
+                //increment counter in modal
+                viewModel.incrementLoadCounter();
+                resolve(results);
+            } else {
+                // reject status upon un-successful status
+                reject(status);
+            }
+        });
+    });
+}
+/* ------------------------------------------------------------------ */
 function placesServiceCallback(results, status) {
     //console.log(results);
     //console.log(status);
@@ -305,13 +360,14 @@ function placesServiceCallback(results, status) {
         }
     }
 }
-
+/* ------------------------------------------------------------------ */
 function createMapMarker(place) {
     var placeLoc = place.geometry.location;
     //console.log(place);
     var marker = new google.maps.Marker({
         map: map,
         //icon: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png",
+        icon: defaultMarkerIcon,
         //animation: google.maps.Animation.DROP,
         position: place.geometry.location
     });
@@ -350,27 +406,35 @@ function createMapMarker(place) {
     infoWindowContent.appendChild(infoWindowImageContainer);
 
     google.maps.event.addListener(marker, 'click', function() {
+        resetMapMarkers();
         infowindow.setContent(infoWindowContent);
+        marker.setIcon(selectedMarkerIcon);
         infowindow.open(map, this);
     });
-
-    
 }
-
+/* ------------------------------------------------------------------ */
 /* If the markers from a place has been cached, display the markers stored in the item's array */
 function displayMapMarkers(name) {
     for(var i = 0; i < placeMarkersCache[name].length; i++) {
         createMapMarker(placeMarkersCache[name][i]);
     }
 }
-
+/* ------------------------------------------------------------------ */
 function clearMapMarkers() {
     for(var i =0; i < currentPlaceMarkers.length; i++) {
         currentPlaceMarkers[i].setMap(null);
     }
 }
-
+/* ------------------------------------------------------------------ */
+function resetMapMarkers() {
+    for(var i =0; i < currentPlaceMarkers.length; i++) {
+        currentPlaceMarkers[i].setIcon(defaultMarkerIcon);
+    }
+}
+/* ------------------------------------------------------------------ */
 $(document).ready(function() {
-    //alert("OK");
-    ko.applyBindings(new AppViewModel());
+    viewModel = new AppViewModel();
+    ko.applyBindings(viewModel);
+    loadInitialPlaces();
 });
+/* ------------------------------------------------------------------ */
